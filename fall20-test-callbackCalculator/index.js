da37dcd95123be5b15b1cc9374ac2b91ec572cda
Sebastien <sebastien.kronlund@gmail.com>
2020-10-03 17:28:44 +0200

    Functionality for promises and async await
    Added functionality for promise based calculations and async await

--- fall20-test-callbackCalculator/index.js
+++ fall20-test-callbackCalculator/index.js
@@ -1,88 +1,139 @@
-// Use this helper to set a timeout in promise
 // eslint-disable-next-line no-unused-vars
 
-const addAwait = () => { throw new Error('Not implemented yet'); };
-
 function verify(num1, num2) {
-  if (Number.isInteger(num1) && Number.isInteger(num2)) {
-    return true;
-  }
-  return false;
+	if (Number.isInteger(num1) && Number.isInteger(num2)) {
+		return true;
+	}
+	return false;
 }
-
 
 // ----------------------------------- Start of callbacks
 function addCallback(number1, number2, cb) {
-  setTimeout(() => {
-    if (verify(number1, number2)) {
-      if (cb && typeof cb === 'function') cb(null, number1 + number2);
-      return number1 + number2;
-    }
-    throw new Error('Something went wrong');
-  }, 100);
+	setTimeout(() => {
+		if (verify(number1, number2)) {
+			if (cb && typeof cb === "function") cb(null, number1 + number2);
+			return number1 + number2;
+		}
+		throw new Error("Something went wrong");
+	}, 100);
 }
 function subtractCallback(number1, number2, cb) {
-  setTimeout(() => {
-    if (verify(number1, number2)) {
-      if (cb && typeof cb === 'function') cb(null, number1 - number2);
-      return number1 - number2;
-    }
-    throw new Error('Something went wrong');
-  }, 100);
+	setTimeout(() => {
+		if (verify(number1, number2)) {
+			if (cb && typeof cb === "function") cb(null, number1 - number2);
+			return number1 - number2;
+		}
+		throw new Error("Something went wrong");
+	}, 100);
 }
 function multiplyCallback(number1, number2, cb) {
-  setTimeout(() => {
-    if (verify(number1, number2)) {
-      if (cb && typeof cb === 'function') cb(null, number1 * number2);
-      return number1 * number2;
-    }
-    throw new Error('Something went wrong');
-  }, 100);
+	setTimeout(() => {
+		if (verify(number1, number2)) {
+			if (cb && typeof cb === "function") cb(null, number1 * number2);
+			return number1 * number2;
+		}
+		throw new Error("Something went wrong");
+	}, 100);
 }
 function divideCallback(number1, number2, cb) {
-  setTimeout(() => {
-    if (verify(number1, number2)) {
-      if (cb && typeof cb === 'function') cb(null, number1 / number2);
-      return number1 / number2;
-    }
-    throw new Error('Something went wrong');
-  }, 100);
+	setTimeout(() => {
+		if (verify(number1, number2)) {
+			if (cb && typeof cb === "function") cb(null, number1 / number2);
+			return number1 / number2;
+		}
+		throw new Error("Something went wrong");
+	}, 100);
 }
 
 // ----------------------------------- End of callbacks
 
-
 // ----------------------------------- Start of promises
-/* const promisifiedSetTimeout = ms => new Promise(resolve => setTimeout(resolve, ms)); */
-function promisifiedSetTimeout(ms) {
-  return new Promise((resolve) => {
-    setTimeout((res) => {
-      resolve("Hello!");
-    }, ms)
-  })
-}
-
-promisifiedSetTimeout(1000).then(res => console.log(res));
+const promisifiedSetTimeout = (ms) =>
+	new Promise((resolve) => setTimeout(resolve, ms));
 
 function addPromise(num1, num2) {
-  return new Promise((res, rej) => {
-    
-    if (verify(num1, num2)) {
-      res(num1 + num2);
-    }
-    rej(new Error('Woops!...'));
-  });
+	return new Promise((res, rej) => {
+		promisifiedSetTimeout(100).then(() => {
+			if (verify(num1, num2)) {
+				res(num1 + num2);
+			}
+			rej(new Error("Woops!..."));
+		});
+	});
 }
 
-addPromise(1, 2)
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+function subtractPromise(num1, num2) {
+	return new Promise((res, rej) => {
+		promisifiedSetTimeout(100).then(() => {
+			if (verify(num1, num2)) {
+				res(num1 - num2);
+			}
+			rej(new Error("Woops!..."));
+		});
+	});
+}
+
+function multiplyPromise(num1, num2) {
+	return new Promise((res, rej) => {
+		promisifiedSetTimeout(100).then(() => {
+			if (verify(num1, num2)) {
+				res(num1 * num2);
+			}
+			rej(new Error("Woops!..."));
+		});
+	});
+}
+
+function dividePromise(num1, num2) {
+	return new Promise((res, rej) => {
+		promisifiedSetTimeout(100).then(() => {
+			if (verify(num1, num2)) {
+				res(num1 / num2);
+			}
+			rej(new Error("Woops!..."));
+		});
+	});
+}
+
+// ----------------------------------- End of promises
+
+// ----------------------------------- Start of async
+
+const addAwait = async (num1, num2) => {
+	await promisifiedSetTimeout(100);
+	if (verify(num1, num2)) return num1 + num2;
+	return new Error("Woops!...");
+};
+
+const subtractAwait = async (num1, num2) => {
+	await promisifiedSetTimeout(100);
+	if (verify(num1, num2)) return num1 - num2;
+	return new Error("Woops!...");
+};
+
+const multiplyAwait = async (num1, num2) => {
+	await promisifiedSetTimeout(100);
+	if (verify(num1, num2)) return num1 * num2;
+	return new Error("Woops!...");
+};
+
+const divideAwait = async (num1, num2) => {
+	await promisifiedSetTimeout(100);
+	if (verify(num1, num2)) return num1 / num2;
+	return new Error("Woops!...");
+};
 
 module.exports = {
-  addCallback,
-  subtractCallback,
-  multiplyCallback,
-  divideCallback,
-  addPromise,
-  addAwait,
+	addCallback,
+	subtractCallback,
+	multiplyCallback,
+	divideCallback,
+	addPromise,
+	subtractPromise,
+	multiplyPromise,
+	dividePromise,
+	addAwait,
+	subtractAwait,
+	multiplyAwait,
+	divideAwait,
 };
